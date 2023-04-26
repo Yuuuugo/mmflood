3ceb9e0fee5c1477cb1b4f45bece30863fb4e25a
Yuuuugo <hugogabrielidis@gmail.com>
2023-04-26 15:08:13 +0200

    [CHANGE] modif

--- train_unconditional.py
+++ train_unconditional.py
@@ -78,7 +78,7 @@ def parse_args():
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="ddpm-model-64",
+        default="saved_model/hugginfacediffusion/",
         help="The output directory where the model predictions and checkpoints will be written.",
     )
     parser.add_argument("--overwrite_output_dir", action="store_true")
